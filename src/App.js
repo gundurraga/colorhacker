7155b99bdb324cfe857852dc14fa90752d59c6cf
gundurraga <67235552+gundurraga@users.noreply.github.com>
2021-07-02 12:36:35 -0400

    2nd test netlify

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>
-        Color Hacker <img style={{ height: "2rem" }} src="./icon.png" />
+        Color Hacker{" "}
+        <img
+          style={{ height: "2rem" }}
+          src="./icon.png"
+          alt="Color Hacker icon"
+        />
       </h1>
       <h2>Learn everything about colors!</h2>
-      <h3>Testing Netlify</h3>
+      <h3>2nd Test Netlify</h3>
     </div>
   );
 }
