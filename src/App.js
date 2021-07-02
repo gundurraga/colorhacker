1ab13a84012ca2a9ad35558f39ac5b091f456b8f
gundurraga <67235552+gundurraga@users.noreply.github.com>
2021-07-02 11:27:09 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Color Hacker &#127752;</h1>
+      <h2>Learn everything about colors!</h2>
+    </div>
+  );
+}
